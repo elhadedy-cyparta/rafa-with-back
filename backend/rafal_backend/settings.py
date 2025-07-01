379b0020f95cad8502cd438ccdf7c9f781c106ab
elhadedy-cyparta <eslamelhadedy@cyparta.com>
2025-07-01 14:17:25 +0300

    Update settings.py

--- backend/rafal_backend/settings.py
+++ backend/rafal_backend/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-key-for-development-o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,d8c8kgcw4gowcscg4s4c40ck.apirafal.cyparta.com").split(",")
 CSRF_TRUSTED_ORIGINS = ["https://d8c8kgcw4gowcscg4s4c40ck.apirafal.cyparta.com","https://*.netlify.app","http://*.netlify.app"]
 
 CORS_ALLOW_ALL_ORIGINS = True
